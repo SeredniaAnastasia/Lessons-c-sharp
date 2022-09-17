f53ac12dbf1d475803421e203a9f5757437aae1a
Serednia Anastasia <anastastasiaseredna.gmail.com>
2022-09-17 21:11:57 +0300

    first task

--- testconsole/Program.cs
+++ testconsole/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class TestClass
+{
+static void Main(string[] args)
+{
+
+   int a1=0, a2=0, a3=0;
+    Console.WriteLine("Hello, World!");
+      a1 = Convert.ToInt32(Console.ReadLine());
+      a2 =a1+5;
+      a3 =a1+5;
+     Console.WriteLine(a1);
+     Console.WriteLine(a2);
+     Console.WriteLine(a3); 
+}
+}
+
