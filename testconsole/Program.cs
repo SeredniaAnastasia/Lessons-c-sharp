197c6d33165cb7b9f6f54325ae3ae7950da2bf4b
Serednia Anastasia <anastastasiaseredna.gmail.com>
2022-09-18 12:02:21 +0300

    chang task1

--- testconsole/Program.cs
+++ testconsole/Program.cs
@@ -5,14 +5,14 @@ class TestClass
 static void Main(string[] args)
 {
 
-   int a1=0, a2=0, a3=0;
-    Console.WriteLine("Hello, World!");
-      a1 = Convert.ToInt32(Console.ReadLine());
-      a2 =a1+5;
-      a3 =a1+5;
-     Console.WriteLine(a1);
-     Console.WriteLine(a2);
-     Console.WriteLine(a3); 
-}
+double z1=0, z2=0, n=0, m=0;
+        Console.WriteLine ("Input n  ");
+        n=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine ("Input m  ");
+        m=Convert.ToInt32(Console.ReadLine());
+        z1=(n-1)*Math.Sqrt(n)-(m-1)*Math.Sqrt(m)/Math.Sqrt(Math.Pow(n,3)*m)+m*n+n*n-n;
+        z2=Math.Sqrt(n)-Math.Sqrt(m)/n;
+        Console.WriteLine ("z1= " + z1);
+        Console.WriteLine ("z2= " + z2);
 }
 
