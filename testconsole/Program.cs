97aa3b41fef4a024123832926924b646619ffa98
Serednia Anastasia <anastastasiaseredna.gmail.com>
2022-09-18 11:58:22 +0300

    chang task main

--- testconsole/Program.cs
+++ testconsole/Program.cs
@@ -4,15 +4,14 @@ class TestClass
 {
 static void Main(string[] args)
 {
-
-   int a1=0, a2=0, a3=0;
-    Console.WriteLine("Hello, World!");
-      a1 = Convert.ToInt32(Console.ReadLine());
-      a2 =a1+5;
-      a3 =a1+5;
-     Console.WriteLine(a1);
-     Console.WriteLine(a2);
-     Console.WriteLine(a3); 
+       int a1 = 0, a2 = 0, a3 = 0;
+        Console.WriteLine("Input first number");
+        a1 = Convert.ToInt32(Console.ReadLine());
+        a2 = a1 + 5;
+        a3 = a1 + 5;
+        Console.WriteLine(a1);
+        Console.WriteLine(a2);
+        Console.WriteLine(a3);
 }
 }
 
