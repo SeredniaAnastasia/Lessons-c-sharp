ebe8e34908cd9311c41d4e38dd48f26962030398
Serednia Anastasia <anastastasiaseredna.gmail.com>
2022-09-25 16:31:01 +0300

    dialog

--- testconsole/Program.cs
+++ testconsole/Program.cs
@@ -1,15 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 class TestClass
 {
-static void Main(string[] args)
-{
-  int r=0; 
-            Console.WriteLine ("Input radius  ");
-         r=Convert.ToInt32(Console.ReadLine());
-         r=r+20;
-         double s=Math.PI*Math.Pow(r,2)-Math.PI*Math.Pow(20,2);
-        Console.WriteLine ("plocha  " + s);
-}
-
+    static void Main(string[] args)
+    {
+        Console.WriteLine ("What is your name?");
+        string name =Console.ReadLine();
+        Console.WriteLine("Hello " +name);
+        
+        Console.WriteLine ("Are you male or female?");
+        string state =Console.ReadLine();
+        Console.WriteLine("I'm a man " );
+       
+        Console.WriteLine ("What is your education?");
+        string education = Console.ReadLine();
+        
+        if(education!="designer")
+        Console.WriteLine("is a useful profession, and I'm studying to be a designer");
+        else 
+        Console.WriteLine("Wondeful, I'm aslo a designer");
+      
+        Console.WriteLine ("Are you happy?");
+        string happy =Console.ReadLine();
+        Console.WriteLine ("Me too ");
+        
+        Console.WriteLine ("Do you like studying?");
+        string job =Console.ReadLine();
+        
+        Console.WriteLine ("What is your favorite book?");
+        string book =Console.ReadLine();
+        Console.WriteLine ("Great, I like Stephen King's books");
+        
+        Console.WriteLine ("Do you do sports?");
+        string sport =Console.ReadLine();
+        Console.WriteLine ("I like to run");
+        Console.WriteLine ("It was nice to meet you, goodbye");   
+    }
 }
